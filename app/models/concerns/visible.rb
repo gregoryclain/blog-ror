39478525f06932689d68158632f56fd2f3a54703
Grégory Clain <g.clain69@gmail.com>
2022-05-28 00:09:19 +0200

    add concerns to visible comments

---
+++ app/models/concerns/visible.rb
@@ -0,0 +1,13 @@
+module visible
+  extend ActiveSupport::Concern
+
+  VALID_STATUSES = ['public', 'private', 'archived']
+
+  included do
+    validates :status, inclusion: { in: VALID_STATUSES }
+  end
+
+  def archived?
+    status == 'archived'
+  end
+end
