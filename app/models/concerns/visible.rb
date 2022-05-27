ae7763e71fc8527c4c178cf88693d452851f3779
Grégory Clain <g.clain69@gmail.com>
2022-05-28 00:37:14 +0200

    improve concern test

--- app/models/concerns/visible.rb
+++ app/models/concerns/visible.rb
@@ -1,4 +1,4 @@
-module visible
+module Visible
   extend ActiveSupport::Concern
 
   VALID_STATUSES = ['public', 'private', 'archived']
@@ -6,6 +6,13 @@ module visible
   included do
     validates :status, inclusion: { in: VALID_STATUSES }
   end
+
+  class_methods do
+    def public_count
+      where(status: 'public').count
+    end
+  end
+
 
   def archived?
     status == 'archived'
